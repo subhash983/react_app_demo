c12e285b81847074e7ecb027314cb63c47d8c573
subhash983 <subhash.983@gmail.com>
2017-02-09 21:46:52 +0530

    updating package.js and other files

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,28 @@
+"use strict";
+
+import React from 'react'
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This application uses the following technologies
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Nodet</li>
+                        <li>Webpackt</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </p>
+            </div>
+        );
+    }
+}
+export default About;
---
+++ src/components/common/header.js
@@ -0,0 +1,30 @@
+"use strict";
+
+import React from 'react';
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <a href="/" className="navbar-brand">
+                        <img src="images/site-logo.png" style={{height:'25px',width:'25px'}}/>
+                    </a>
+                    <ul className="nav navbar-nav">
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/#about">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Header;
